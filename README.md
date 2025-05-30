1de4333ecb05cedef2df33949886c6c05e729986
FelTap <felipeTapia1223@gmail.com>
2025-05-30 13:41:48 -0700

    feat(main) Add a post endpoint to make completions and python logger.

--- README.md
+++ README.md
@@ -5,5 +5,9 @@
 ##How to run the app 
 1. Activate the virtual environment
 2. pip install -r requirements.txt
-3. python -m uvicorn main:app --reload install
-4. curl http://localhost:8000
+3. python -m uvicorn main:app --reload
+4. Check the hello world
+curl http://localhost:8000
+
+5. To test completion API please set the prompt value
+curl -i -H "Content-Type: application/json" -X POST  http://localhost:8000/completion -d '{ "prompt" : "How is the president of Mexico" }'
--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents import BaseDocumentTransformer, Document
 from fastapi import FastAPI
+from choya.model.completion import CompletionRequest,CompletionResponse
+
+import logging
+logger = logging.getLogger("maru_ai_main")
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
 
 def rag_logic_here():
     pdf = PdfReader("test/resources/ley_agraria.pdf")
@@ -10,11 +22,11 @@ def rag_logic_here():
         text += page.extract_text()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 2000,chunk_overlap =100)
     chunks = text_splitter.split_text(text)
-    print(chunks[0])
-    print("*"*100)
-    print(chunks[1])
-    print("*"*100)
-    print(chunks[2])
+    logger.info(chunks[0])
+    logger.info("*"*100)
+    logger.info(chunks[1])
+    logger.info("*"*100)
+    logger.info(chunks[2])
 
 #Creation of the fast api object
 app = FastAPI()
@@ -25,3 +37,8 @@ async def root():
     return {
         "message" : "Guoyootlot"
     }
+
+@app.post("/completion", response_model=CompletionResponse)
+async def completion(body : CompletionRequest):
+    logger.info(f"Completion request received {body}")
+    return  CompletionResponse(output="Hi How can I help you")
