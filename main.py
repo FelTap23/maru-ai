af79c3cdc34d846a3aedcb4c076277f52d7674d0
FelTap <felipeTapia1223@gmail.com>
2025-05-30 12:29:30 -0700

    My first endpoint

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents import BaseDocumentTransformer, Document
+from fastapi import FastAPI
 
-pdf = PdfReader("test/resources/ley_agraria.pdf")
+def rag_logic_here():
+    pdf = PdfReader("test/resources/ley_agraria.pdf")
+    text = ""
+    for page in pdf.pages:
+        text += page.extract_text()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 2000,chunk_overlap =100)
+    chunks = text_splitter.split_text(text)
+    print(chunks[0])
+    print("*"*100)
+    print(chunks[1])
+    print("*"*100)
+    print(chunks[2])
 
-text = ""
-for page in pdf.pages:
-    text += page.extract_text()
+#Creation of the fast api object
+app = FastAPI()
 
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 2000,chunk_overlap =100)
-chunks = text_splitter.split_text(text)
-
-
-
-print(chunks[0])
-print("*"*100)
-print(chunks[1])
-print("*"*100)
-print(chunks[2])
+@app.get("/")
+async def root():
+    return {
+        "message" : "Guoyootlot"
+    }
